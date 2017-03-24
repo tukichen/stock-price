dc75e896c27ecd199140093d0ab77d7490ba0a0b
tukichen <qiaolinchen@QC.local>
2017-03-24 00:16:32 -0400

    stock

--- app.py
+++ app.py
@@ -28,22 +28,72 @@ def index():
     if request.method == 'GET':
         return render_template('userinfo.html' )
     else:        #request was a POST
-        # name is a list of stock tickers
-        app.vars['name'] = request.form['name'].split(',')
+        # name is a list of stock tickers, change to upper case and split into a list
+        app.vars['name'] = request.form['name'].upper().split(',')
         # input_faeture = open, close, high, low
         app.vars['input_feature'] = []
-        if request.form['open']:  app.vars['input_feature'].append(' - Open')
-        if request.form['close']: app.vars['input_feature'].append(' - Close')
-        if request.form['high']:  app.vars['input_feature'].append(' - High')
-        if request.form['low']:   app.vars['input_feature'].append(' - Low')
+        if request.form.get('open'):  app.vars['input_feature'].append(' - Open')
+        if request.form.get('close'): app.vars['input_feature'].append(' - Close')
+        if request.form.get('high'):  app.vars['input_feature'].append(' - High')
+        if request.form.get('low'):   app.vars['input_feature'].append(' - Low')
+    
+
         
+        tickers = app.vars['name']        # A list of tickers
+        input_feature = app.vars['input_feature']   # A list of features to show
+        # create a list of dataset names
+        dset =[]
+        for ticker in tickers:
+            dset.append("WIKI/"+ ticker +"-Open")
+        
+        #get stock data in pandas dataframe
+        stock = quandl.get(dataset= dset,  api_key= 'wad4CxZw1s-6BTGhjei6', returns= 'pandas' )
+        # column names to select from pandas data
+        features =[]
+        for ds in dset:
+            for f in input_feature:
+                features.append(ds + f)
+
+        # select only features wanted
+        stock_data= stock.loc[:, features]
+        stock_data['Date'] = stock_data.index.values.astype('datetime64[ns]')
+
+        color_list = ['#A6CEE3','#B2DF8A','#33A02C','#FB9A99','blue']  # list 4 colors for lines
+        plot_list =[]   # empty list of plots
+    
+
+
+
+        for x in tickers:
+            globals()['p%s' % x] = figure(x_axis_type="datetime", title="Stock Prices: "+x)
+            globals()['p%s' % x].grid.grid_line_alpha=0.3
+            globals()['p%s' % x].xaxis.axis_label = 'Date'
+            globals()['p%s' % x].yaxis.axis_label = 'Price'
+            globals()['p%s' % x].ygrid.band_fill_color = "olive"
+            globals()['p%s' % x].ygrid.band_fill_alpha = 0.04
+    
+            for f in input_feature :
+                i= input_feature.index(f)
+                #print(i, x, f)
+                globals()['p%s' % x].line(stock_data['Date'], stock_data['WIKI/'+x+'-Open'+f], color= color_list[i], legend= x+f)
+    
+            globals()['p%s' % x].legend.location = "top_left"
+    
+            plot_list.append(globals()['p%s' % x])
+
+        '''#----------------------------------------
         f = open('%s_price.txt'%(app.vars['name']),'w')
         f.write('Name: %s\n'%(app.vars['name']))
         f.write('price'+ '|'.join(app.vars['input_feature'])+'\n')
+        f.write('column'+ '|'.join(features)+'\n')
+        f.write('plot'+ '|'.join(plot_list)+'\n')
         f.close()
-        
-        
-        
+        #---------------------------------------'''
+        output_file("./templates/stocks.html", title="Stock Price")
+
+        show(gridplot([plot_list], plot_width=600, plot_height=600))  # open a browser
+
+
         #-------------------return html plot from bokeh----------------------
         return render_template('stocks.html')
 
